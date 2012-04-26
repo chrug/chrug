a906418617135274aad3b5a5a5a0b4a36e19119d
Micah Woods <micahwoods@gmail.com>
2012-04-26 14:12:29 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chrug::Application.config.secret_token = 'beb09b625f423aa505d2421cb9585a7dba44b68c704afe0d9d4edb675b9006a4cb519315a8a0967b990e3f37bd6b530e76d1e2c61579be4b069a4c98356d845a'
